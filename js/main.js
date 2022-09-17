73d30ecb6168bb81d06de1f30985b1fff18551d1
Yesy Magana <yesy.magana@yahoo.com>
2022-09-16 17:17:05 -0700

    formatting

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-// --------------- PREHISTORIC MODE ---------------
-
 const dino = document.getElementById('dino');
 const cactus = document.getElementById('cactus');
 
@@ -13,10 +11,7 @@ function jump() {
     }
 }
 
-document.addEventListener('keydown', function(event) {
-    jump();
-    event.preventDefault();
-});
+
 
 let isAlive = setInterval(function() {
     // get current dino Y position
@@ -31,73 +26,44 @@ let isAlive = setInterval(function() {
     }
 }, 10);
 
+document.addEventListener('keydown', function(event) {
+    jump();
+    event.preventDefault();
+});
+
 // ---------- Button/Toggle Event Listener -----------
 
 document.querySelector('button').addEventListener('click', () => {
     document.body.classList.toggle('dark');
 
     const dinopool = document.getElementById('dinopool');
-const cow = document.getElementById('cow');
+    const cow = document.getElementById('cow');
 
-function jump() {
-    if (dinopool.classList != 'jump'){
-        dinopool.classList.add('jump');
+    function jump() {
+        if (dinopool.classList != 'jump'){
+            dinopool.classList.add('jump');
 
-        setTimeout(function() {
-            dinopool.classList.remove('jump');
-        }, 400);
+            setTimeout(function() {
+                dinopool.classList.remove('jump');
+            }, 400);
+        }
     }
-}
 
-document.addEventListener('keydown', function(event) {
-    jump();
-    event.preventDefault();
-});
+    document.addEventListener('keydown', function(event) {
+        jump();
+        event.preventDefault();
+    });
 
-let isDinopoolAlive = setInterval(function() {
-    // get current dino Y position
-    let dinopoolTop = parseInt(window.getComputedStyle(dinopool).getPropertyValue('top'));
+    let isDinopoolAlive = setInterval(function() {
+        // get current dino Y position
+        let dinopoolTop = parseInt(window.getComputedStyle(dinopool).getPropertyValue('top'));
 
-    // get current cow X position
-    let cowLeft = parseInt(window.getComputedStyle(cow).getPropertyValue('left'));
+        // get current cow X position
+        let cowLeft = parseInt(window.getComputedStyle(cow).getPropertyValue('left'));
 
-    // detect collision
-    if (cowLeft < 50 && cowLeft > 0 && dinopoolTop >= 140) {
-       //alert('GAME OVER');
-    }
-}, 10)
+        // detect collision
+        if (cowLeft < 50 && cowLeft > 0 && dinopoolTop >= 140) {
+        alert('GAME OVER');
+        }
+    }, 10)
 }); 
-
-
-// --------------- DEADPOOL MODE ---------------
-
-// const dinopool = document.getElementById('dinopool');
-// const cow = document.getElementById('cow');
-
-// function jump() {
-//     if (dinopool.classList != 'jump'){
-//         dinopool.classList.add('jump');
-
-//         setTimeout(function() {
-//             dinopool.classList.remove('jump');
-//         }, 400);
-//     }
-// }
-
-// document.addEventListener('keydown', function(event) {
-//     jump();
-//     event.preventDefault();
-// });
-
-// let isDinopoolAlive = setInterval(function() {
-//     // get current dino Y position
-//     let dinopoolTop = parseInt(window.getComputedStyle(dinopool).getPropertyValue('top'));
-
-//     // get current cow X position
-//     let cowLeft = parseInt(window.getComputedStyle(cow).getPropertyValue('left'));
-
-//     // detect collision
-//     if (cowLeft < 50 && cowLeft > 0 && dinopoolTop >= 140) {
-//        //alert('GAME OVER')
-//     }
-// }, 10)
